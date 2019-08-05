3992800cf9b98f1daf39900f11f841eab416a076
tbrams <torben@brams.dk>
2019-08-05 19:56:40 +0200

    Forgot this

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -6,20 +6,24 @@ class CategoryMealsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final routeArgs = ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
-    final categoryMeals = DUMMY_MEALS.where((meal){
-       return meal.categories.contains(categoryId);
-     }).toList();
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: ListView.builder(itemBuilder: (ctx, index){
-        return Text(categoryMeals[index].title);
-      }, itemCount: categoryMeals.length ,),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
+      ),
     );
   }
 }
