37c3dd5e628ac5d6a67108d97630033822f4bf24
tbrams <torben@brams.dk>
2019-08-06 15:23:03 +0200

    updating screen title when switching tabs

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -8,30 +8,40 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = [
+    {'page' : CategoriesScreen(), 'title': 'Categories'},
+    {'page' : FavouriteScreen(), 'title': 'Your Favourites'},
+  ];
+  int _selectedPageIndex=0;
+
+  void _selectPage(int index){
+    setState(() {
+      _selectedPageIndex=index; 
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Meals'),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categories',
-              ),
-              Tab(icon: Icon(Icons.star), text: 'favourites'),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            CategoriesScreen(),
-            FavouriteScreen(),
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title']),
       ),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: _selectPage,
+          backgroundColor: Theme.of(context).primaryColor,
+          unselectedItemColor: Colors.white,
+          selectedItemColor: Theme.of(context).accentColor,
+          currentIndex: _selectedPageIndex,
+          items: [
+             BottomNavigationBarItem(
+              icon: Icon(Icons.category),
+              title: Text('Categories'),
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.favorite), title: Text('Favourites')),
+          ]),
     );
   }
 }
